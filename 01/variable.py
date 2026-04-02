679ab7d359593dd9bf2cadfbd07b328a9df4402b
kiranbtp-sap <kiransapbtp@gmail.com>
2026-04-02 12:51:00 +0530

    chnages 2

---
+++ 01/variable.py
@@ -0,0 +1,33 @@
+#scaler variable this store only single value
+
+# string varibale 
+
+name = "kiran"
+
+#intiger variable 
+
+age = 30
+
+#float variable 
+height  = 5.9
+
+# boollen variabl 
+is_stuents  = False
+
+print("name: ", name, "is of type ", type(name))
+print("age : ", age, "is of type ", type(age))
+print("height : ", height, "is of type ", type(height))
+print("is student : ", is_stuents, "is of type ", type(is_stuents))
+
+
+newage = input("Enter your age : ")
+
+if int(newage) > 18 :
+    print(name,"is an adult")
+elif int(newage) == 18:
+     print(name,"is just an adult")
+else: 
+     print(name,"is not an adult")
+
+
+
